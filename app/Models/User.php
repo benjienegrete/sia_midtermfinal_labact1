e6f53ebb769d213ab14c7d879c54c6573a53f9bd
Benjie09 <benjienegrete35@gmail.com>
2023-05-20 20:08:08 +0800

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class User extends Model{
 
-    protected $table = 'tbl_user';
+    protected $table = 'tbl_user2';
     protected $fillable = [
         'username', 'password', 'gender'
     ];
